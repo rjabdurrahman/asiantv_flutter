4987692fd0a141ded7e7fa6343af69d33b577ad2
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-10-08 12:53:06 +0600

    Added: Loading in category vid list

--- lib/pages/category-video-list.dart
+++ lib/pages/category-video-list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:tv_app/models/model-videos.dart';
 import 'package:tv_app/services/service-video.dart';
 import 'package:tv_app/widgets/recent-videos.dart';
@@ -40,7 +41,33 @@ class _CategoryVideoListState extends State<CategoryVideoList> {
   Widget build(BuildContext context) {
     return Container(
       child: _loading
-          ? Text('Loading..' + widget.categoryName)
+          ? Shimmer.fromColors(
+              child: GridView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.vertical,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      crossAxisSpacing: 5.0,
+                      mainAxisSpacing: 5.0,
+                      childAspectRatio: 2.7),
+                  itemBuilder: (context, index) => Material(
+                        //  Starting creating widget for recent videos
+                        color: Colors.white,
+                        elevation: 14.0,
+                        borderRadius: BorderRadius.circular(24.0),
+                        shadowColor: Color(0x802196F3),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              child:
+                                  Text(widget.categoryName + " is Loading.."),
+                            )
+                          ],
+                        ),
+                      )),
+              baseColor: Colors.grey,
+              highlightColor: Colors.redAccent)
           : RecentVideos(videos),
     );
   }
